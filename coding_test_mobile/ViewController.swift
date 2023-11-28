4532bec9a3b33234311be0e518149339f0311f78
ejkim-ip <ejkim@ipixelcorp.com>
2023-11-29 08:44:43 +0900

    Initial Commit

---
+++ coding_test_mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coding_test_mobile
+//
+//  Created by eunjin kim on 2023/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ coding_test_mobileUITests/coding_test_mobileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  coding_test_mobileUITestsLaunchTests.swift
+//  coding_test_mobileUITests
+//
+//  Created by eunjin kim on 2023/11/29.
+//
+
+import XCTest
+
+final class coding_test_mobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
